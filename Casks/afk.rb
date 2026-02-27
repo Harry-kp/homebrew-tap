4c6c09038dce475b94a096383cbaaefd161dca36
Harry-kp <chaudharyharshit9@gmail.com>
2026-02-28 01:35:01 +0530

    fix: point afk cask and workflows to Harry-kp/afk instead of afk-releases
    Repo is now public, no need for a separate releases repo.

--- Casks/afk.rb
+++ Casks/afk.rb
@@ -2,7 +2,7 @@ cask "afk" do
   version "1.1.0"
   sha256 "c7ba83dcad924473d81578980f4b1857afc07b692a1a5ec63ca72bbe63b454a0"
 
-  url "https://github.com/Harry-kp/afk-releases/releases/download/v#{version}/Afk_#{version}_universal.dmg"
+  url "https://github.com/Harry-kp/afk/releases/download/v#{version}/Afk_#{version}_universal.dmg"
   name "Afk"
   desc "Break reminder app - step away from your keyboard"
   homepage "https://afk-app.vercel.app"
